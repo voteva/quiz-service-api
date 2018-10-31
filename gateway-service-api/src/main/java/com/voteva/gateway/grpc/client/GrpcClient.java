f467947bc0e9c4f726c104ee3790fdf32067b4ed
t.voteva <t.voteva@qiwi.com>
2018-10-31 23:19:52 +0300

    Refactor

---
+++ gateway-service-api/src/main/java/com/voteva/gateway/grpc/client/GrpcClient.java
@@ -0,0 +1,15 @@
+package com.voteva.gateway.grpc.client;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface GrpcClient {
+
+    String serviceName() default "";
+}
