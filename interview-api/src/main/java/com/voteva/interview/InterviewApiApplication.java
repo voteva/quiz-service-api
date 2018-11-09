493f9c5eeaf4d75b2ec61fb75acb7726c7845f8b
t.voteva <t.voteva@qiwi.com>
2018-11-09 16:48:35 +0300

    Refactor

---
+++ interview-api/src/main/java/com/voteva/interview/InterviewApiApplication.java
@@ -0,0 +1,12 @@
+package com.voteva.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewApiApplication.class, args);
+    }
+}
